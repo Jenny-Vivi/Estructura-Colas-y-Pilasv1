c987cf9e5386ca6dfe67b59f16333c44a06f9406
Viviana Carchi <jcarchif@est.ups.edu.ec>
2024-06-09 20:31:49 -0500

    Estructura datos Lineas - Pilas

---
+++ README.md
@@ -0,0 +1,35 @@
+# Pilas y Colas
+Version de TAG: v2.0.0 
+Mensaje de TAG: "Estructura datos Lineas - Pilas
+
+# Pilas  y Colas Genericas
+Comit:
+Mensaje: ADD: Clases Cola
+Tag
+Version: v2.0.1
+Mensaje: "Estructura datos Lineas - Colas" 
+
+# Ejercicio_01_sign
+Validación de Signos
+Implemento en un algoritmo para determinar si un "string" quecontiene los caracteres '(', ')', '{', '}', '[' y ']' es 
+válido. 
+Por ejemplo en el main:
+o Input: "([]){}"
+o Si es correcto entonces enviara un mensaje: true
+• Ejemplo 2:
+o Si no es correcto estos caracteres: "({)}"
+o se enviara un mensaje boolean: false
+
+# Ejercicio_02_sorting
+Ordenar una pila, en donde se implementara que se ordene la pila, los elementos más pequeños queden en el tope 
+del Stack. Se utiliza una pila auxiliar que se llmara auxPila.
+Ejemplo:
+• Input: (tope) 5->1->4->2
+• Output: (tope) 1->2->4->5
+
+La version sera de: 2.0.2
+
+# Hecho por:
+- Carchi Viviana
+- Coronel Luis
+xd
---
+++ src/App.java
@@ -0,0 +1,113 @@
+import Materia.Models.Pantallas;
+import Materia.Pilas.PilaGenerica;
+import Materia.Pilas.Pilas;
+
+import java.util.Stack;
+
+import Materia.Queue;
+import Materia.QueueGen;
+import Materia.Ejercicio_01_sign.SignValidator;
+
+import Materia.Ejercicio_02_sorting.StackSorte;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("¡Hola, Mundo!");
+        // ---------------------------------------------------------------------------------------
+        Pilas pila = new Pilas();
+        pila.push(10);
+        pila.push(20);
+        pila.push(30);
+        pila.push(40);
+        pila.push(50);
+        pila.push(60);
+        pila.push(70);
+        // ---------------------------------------------------------------------------------------
+        System.out.println("Elemento en la cima de la pila es: " + pila.peek());
+        System.out.println("Elemento eliminado de la pila es: " + pila.pop());
+        System.out.println("Elemento en la cima de la pila es: " + pila.peek());
+        // ---------------------------------------------------------------------------------------
+        System.out.println();
+        // Implementacion de pila generica.
+        PilaGenerica<Pantallas> pilaPantallas = new PilaGenerica<>();
+        pilaPantallas.push(new Pantallas("Home Page", "/home"));
+        pilaPantallas.push(new Pantallas("Menu Page", "/home/menu"));
+        pilaPantallas.push(new Pantallas("Settings Page", "/home/menu/settings"));
+        // ---------------------------------------------------------------------------------------
+        System.out.println("Estoy en la pantalla: \n\t ----> " + pilaPantallas.peek().getNombre());
+        System.out.println("Destruir pantalla: \n\t ----> " + pilaPantallas.pop().getNombre());
+        System.out.println("Estoy en la pantalla: \n\t ----> " + pilaPantallas.peek().getNombre());
+        pilaPantallas.push(new Pantallas("User Page", "/home/menu/user"));
+        System.out.println("Estoy en la pantalla: \n\t ----> " + pilaPantallas.peek().getNombre());
+        // ---------------------------------------------------------------------------------------
+        // Implementación de de la cola generica tipo pantalla.
+        Queue queue = new Queue();
+        // ----------------------------------------------------------
+        queue.addNode(10);
+        queue.addNode(20);
+        queue.addNode(30);
+        // ----------------------------------------------------------
+        // Mostrar el elemento en el frente.
+        System.out.println("Elemento en el frente: " + queue.peek());
+        // ----------------------------------------------------------
+        // Retirar elementos de la cola.
+        System.out.println("Elemento retirado[1]: " + queue.remove());
+        System.out.println("Elemento en el frente[1]: " + queue.peek());
+        // -------------------------------------------------------------
+        System.out.println("Elemento retirado[2]: " + queue.remove());
+        System.out.println("Elemento en el frente[2]: " + queue.peek());
+
+        // Verificar si la cola está vacía
+        System.out.println("¿Cola vacía: " + (queue.isEmpty() ? "Si" : "No"));// False
+
+        QueueGen<Pantallas> queueGen = new QueueGen<>();
+        queueGen.addNode(new Pantallas("1", "Home Page"));
+        queueGen.addNode(new Pantallas("2", "Menu Page"));
+        queueGen.addNode(new Pantallas("3", "Settings"));
+
+        System.out.println("Estoy en la pantalla \t" + queueGen.peek().getNombre());
+        System.out.println("Pantalla destruida \t" + queueGen.remove().getNombre());
+
+        queueGen.addNode(new Pantallas("4", "User Page"));
+        System.out.println("Estoy en la pantalla \t" + queueGen.peek().getNombre());
+        System.out.println("Pantalla destruida \t" + queueGen.remove().getNombre());
+        System.out.println("Estoy en la pantalla \t" + queueGen.peek().getNombre());
+        System.out.println("El tamaño es: " + queueGen.size());
+
+        //EJERCICIO 1:
+
+        //Ejemplo1
+        SignValidator validador = new SignValidator();
+        String input = "([]){}";
+        boolean result1=  validador.isValid(input);
+        System.out.println("INPUT 1: " + input + "\n"+"The sign validator returns:  " + result1);
+
+        //Ejemplo2
+        String input2="({)}";
+        boolean result2=validador.isValid(input2);
+        System.out.println("INPUT 2: "+input2+ "\n"+"The sign validator returns: "+result2);
+
+        //EJERCICIO 2:
+
+        //Ejemplo1
+       
+        Stack<Integer> stack = new Stack<>();
+        stack.push(5);
+        stack.push(1);
+        stack.push(4);
+        stack.push(2);
+
+        System.out.println("INPUT (tope): " + stack);
+        StackSorte.sortStack(stack);
+        System.out.println("La Pila Ordenado es: " + stack);
+        System.out.print("OUTPUT(tope) is: ");
+        while (!stack.isEmpty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+}
+       
+      
+       
+    
+
